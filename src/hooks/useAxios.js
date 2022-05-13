14d7125d190a0bbaf89b60560af2ba2ba271a6cf
dev.k <dev.krecord@gmail.com>
2022-05-13 19:25:57 +0900

    feat: custom hooks useAxios 만들기

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,3 +1,40 @@
-export const useAxios = () => {
-  return <></>;
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState } from 'react';
+import { useEffect } from 'react';
+import defaultAxios from 'axios'; // yarn add axios
+
+// axios 는 http request를 만들어준다.
+export const useAxios = (opts, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+  const [trigger, setTrigger] = useState(0);
+  if (!opts.url) {
+    return;
+  }
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    setTrigger(Date.now()); // 현재 시각으로 state를 업데이트해서 리렌더링
+  };
+  // trigger가 업데이트 될 때마다 실행
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        // error 처리
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+  return { ...state, refetch };
 };
--- src/pages/UseAxios.js
+++ src/pages/UseAxios.js
@@ -1,6 +1,21 @@
 import { useAxios } from '../hooks/useAxios';
+
 const UseAxios = () => {
-  return <></>;
+  const options = {
+    url: 'https://yts.mx/api/v2/list_movies.json', // example url
+  };
+
+  const { loading, data, refetch } = useAxios(options);
+  console.log('data', data);
+  return (
+    <div className="App">
+      <h2 className="subTitle">useAxios</h2>
+      <h3>[ HTTP Statu ]</h3>
+      <h3>{data && data.status}</h3>
+      <h3>{loading && 'Loading'}</h3>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
 };
 
 export default UseAxios;
